03cec297005ae608c74a43dfc614e5ecf6b6e989
Mateen <mateenitsu@gmail.com>
2018-01-13 08:13:46 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mateen/asynctasklab/Event.java
@@ -0,0 +1,18 @@
+package com.example.mateen.asynctasklab;
+
+/**
+ * Created by Mateen on 1/13/2018.
+ */
+
+public class Event {
+
+    private int percentage;
+
+    public Event(int percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+}
